9434384b0e75e071eb9250b0ebbaffd9b6cff271
GhayasAwan <ghayasawan662@gmail.com>
2026-08-23 14:20:08 +0500

    Enable direct Google Fonts stylesheet with display=swap and universal React hydration for instant FCP on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,18 @@ import App from './App.jsx'
 import './assets/css/style.css'
 import './assets/css/icons.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
